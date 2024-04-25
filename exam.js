b1caa28c938ca25ba7278bdf3ec1a5ec3dc7d7d6
TanyaVerb <karra_t@mail.ru>
2024-04-25 22:04:08 +0300

    fix  function displayEmployeeInfo

--- exam.js
+++ exam.js
@@ -1,70 +1,78 @@
-
-
 // _________________________________________________________
-const task = [ 'проснуться', 'поработать', 'погулять', 'поесть']
+const task = ["проснуться", "поработать", "погулять", "поесть"];
 
-function updateTask(tasks,startIndex, newTask,  deleteCount) {
- tasks.splice(startIndex,deleteCount,newTask )
+function updateTask(tasks, startIndex, newTask, deleteCount) {
+  tasks.splice(startIndex, deleteCount, newTask);
   return task;
 }
 
-console.log('11111111', updateTask(task, 0, 'поспать'))
+console.log("11111111", updateTask(task, 0, "поспать"));
 
 // ________________________________________________________
 
-const  task1 = ['проснуться', 'поработать', 'погулять', 'поесть']
+const task1 = ["проснуться", "поработать", "погулять", "поесть"];
 
-function modifyTaskList(tasks, indexToRemove, taskToAdd, indexToReplece, newTask){
-    //удаление задачи по индексу
-    tasks.splice(indexToRemove,1)
-    //добавление новой задачи
-    tasks.splice(0,0,taskToAdd)
-    // добавление новой задачи с заменой существующей
-    tasks.splice(indexToReplece,0,newTask )
-    return task1;
+function modifyTaskList(
+  tasks,
+  indexToRemove,
+  taskToAdd,
+  indexToReplece,
+  newTask
+) {
+  //удаление задачи по индексу
+  tasks.splice(indexToRemove, 1);
+  //добавление новой задачи
+  tasks.splice(0, 0, taskToAdd);
+  // добавление новой задачи с заменой существующей
+  tasks.splice(indexToReplece, 0, newTask);
+  return task1;
 }
 
-console.log (modifyTaskList(task1,3, 'learn js',1 , 'отдыхать'))
+console.log(modifyTaskList(task1, 3, "learn js", 1, "отдыхать"));
 
 //_________________________________________________________________
 
-let shoppingList = [ 'хлеб', 'молоко', 'яблоки', 'яйца', 'масло', ' сахар', 'мука']
+let shoppingList = [
+  "хлеб",
+  "молоко",
+  "яблоки",
+  "яйца",
+  "масло",
+  "сахар",
+  "мука",
+];
 
-function copyAndExtractList(shoppingList, startIndex, endIndex ){
-    let copy = shoppingList.slice();
-    let extracted = shoppingList.slice(startIndex, endIndex)
-return {copy, extracted};
+function copyAndExtractList(shoppingList, startIndex, endIndex) {
+  let copy = shoppingList.slice();
+  let extracted = shoppingList.slice(startIndex, endIndex);
+  return { copy, extracted };
 }
 
-console.log (copyAndExtractList(shoppingList, 2, 6 ))
+console.log(copyAndExtractList(shoppingList, 2, 6));
 
 //___________________________________________________________________
 
 const articles = [
-    {title: 'bbbbbbb', views:700},
-    {title: 'bbbbbbb', views:500},
-    {title: 'bbbbbbb', views:400},
-    {title: 'bbbbbbb', views:500},
-    {title: 'bbbbbbb', views:200},
+  { title: "bbbbbbb", views: 700 },
+  { title: "bbbbbbb", views: 500 },
+  { title: "bbbbbbb", views: 400 },
+  { title: "bbbbbbb", views: 500 },
+  { title: "bbbbbbb", views: 200 },
+];
 
+function getPopularArticles(articles, count) {
+  let copyArticles = articles.sort(function (a, b) {
+    return a.views - b.views;
+  });
 
-        
-]
+  let newarr = copyArticles.slice(0, count);
 
-function getPopularArticles(articles, count){
-    let  copyArticles = articles.sort(function(a,b){
-    return  a.views  -  b.views 
-    })
-    
-    let newarr = copyArticles.slice (0,count);
-    
-    return newarr;
-} 
+  return newarr;
+}
 
-
-console.log(getPopularArticles(articles,  3))
-console.log(getPopularArticles(articles,  4))
-console.log(getPopularArticles(articles,  4))
+console.log(getPopularArticles(articles, 3));
+console.log(getPopularArticles(articles, 4));
+console.log(getPopularArticles(articles, 4));
 
 /*Напишите функцию displayEmployeeInfo(employees), которая принимает массив объектов employees и
 выводит информацию о каждом сотруднике в консоль.
@@ -77,18 +85,20 @@ console.log(getPopularArticles(articles,  4))
 // ];*/
 
 const employees = [
-        { name: 'Иван', position: 'Разработчик', salary: 3000 },
-        { name: 'Мария', position: 'Дизайнер', salary: 2500 },
-         { name: 'Алексей', position: 'Тестировщик', salary: 2800 }
-]
- function displayEmployeeInfo(employees){
-    let result = employees.forEach(element => {
-        console.log(element.name)
-        console.log(element.position)
-        console.log(element.salary)
-        console.log(`&{element.name} +'должность'+ &{element.position}  + 'зарплата' + &{element.salary} `)
-    });
-    return result
- };
+  { name: "Иван", position: "Разработчик", salary: 3000 },
+  { name: "Мария", position: "Дизайнер", salary: 2500 },
+  { name: "Алексей", position: "Тестировщик", salary: 2800 },
+];
+function displayEmployeeInfo(employees) {
+  let result = employees.forEach((element) => {
+    console.log(element.name);
+    console.log(element.position);
+    console.log(element.salary);
+    console.log(
+      ` Имя: ${element.name}, должность: ${element.position},  зарплата: ${element.salary} `
+    );
+  });
+  return result;
+}
 
- displayEmployeeInfo(employees)
+displayEmployeeInfo(employees);
